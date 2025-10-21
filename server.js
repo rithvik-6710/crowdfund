a9cd667cf77a59ba075ee72751b78cf730c14e12
unknown <bantaramrithviksai522@gmail.com>
2025-10-21 23:49:28 +0530

    initial all commits

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-const Project = require('./models/Project');
+const Project = require("./models/project");
 
 const app = express();
 
@@ -63,5 +63,5 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
