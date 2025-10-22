b3396148d8b8002ad73cd6848cd19cd0efb8a8bf
unknown <bantaramrithviksai522@gmail.com>
2025-10-22 22:33:21 +0530

    initialized all files and committed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-const Project = require('./models/Project');
+const Project = require('./models/project');
 
 const app = express();
 
